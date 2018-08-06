22999069109d2d6c069f0bce5bf47842048ba3e9
Albert Sare <thesundaymailman@gmail.com>
2018-08-06 12:34:28 -0700

    re-configuring cors

--- server.js
+++ server.js
@@ -33,20 +33,20 @@ app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
 }));
 
 // CORS
-// app.use(
-//   cors({
-//     origin: CLIENT_ORIGIN
-//   })
-// );
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  next();
-});
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(204);
+//   }
+//   next();
+// });
 
 // API routes
 app.use('/api', authRouter);
