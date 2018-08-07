613cf45fa6e3f2e4c552321b44a039951eae069f
Albert Sare <thesundaymailman@gmail.com>
2018-08-07 11:41:35 -0700

    refined questions POST router

--- routes/questions.js
+++ routes/questions.js
@@ -28,15 +28,14 @@ router.post('/', (req, res, next) => {
   const { input, id } = req.body;
   return Question
     .findById(id)
-    .then(res=>res.json(res))
-    .then(res=>res.name === input ? {bool: true, name: res.name}  : {bool: false, name: res.name})
+    .then(result => result.name === input ? res.json({bool: true, name: result.name}) : res.json({bool: false, name: result.name}))
     .catch(err => {
       // Forward validation errors on to the client, otherwise give a 500
       // error because something unexpected has happened
       if (err.reason === 'ValidationError') {
         return res.status(err.code).json(err);
       }
-      res.status(500).json({code: 500, message: 'Internal server error'})
+      res.status(500).json({code: 500, message: 'Internal server error'});
     });
 });
 
