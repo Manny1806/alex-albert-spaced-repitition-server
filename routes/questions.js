c279263372d68d00c4de2bb59b867f4de34a9615
Albert Sare <thesundaymailman@gmail.com>
2018-08-08 12:19:44 -0700

    implementing algorithm

--- routes/questions.js
+++ routes/questions.js
@@ -15,10 +15,56 @@ router.post('/', jwtAuth, (req, res, next) => {
   console.log(id);
   return User
     .findById(id)
-    .then(user => res.json(user.questions[0]))
+    .then(user => res.json(user.questions[user.head]))
     .catch(err => next(err));
 });
 
+/* ========== GET ANSWER FOR A POKEMON ========== */
+router.post('/:id', jwtAuth, (req, res, next) => {
+  const { id } = req.params;
+  const { input, userId } = req.body;
+  return Question
+    .findById(id)
+    // .then(result => result.name === input ? res.json({bool: true, name: result.name}) : res.json({bool: false, name: result.name}))
+    .then(result => result.name === input ? {isCorrect: true, answer: result.name} : {isCorrect: false, answer: result.name})
+    .then(answer => {
+      if (answer.isCorrect) {
+        // update user's list of questions
+        // call up user's list of question, modify
+        User
+          .findById(userId)
+          .then(user => {
+            // already know where we are based on head
+            // find insertion point, which is question[newMemValue]
+            // so question[newMemValue].next changes to currHead
+            let currHead = user.questions[user.head];
+            let currMemValue = user.questions[user.head].memoryStrength;
+            let currNext = user.questions[user.head].next;
+            let nextQuestion;
+
+            currMemValue *= 2;
+            currHead = currNext;
+            currNext;
+
+            // return User.findAndUpdate()
+          })
+          .catch();
+        // send user feedback using res.json
+      } else {
+        // update user's list of questions
+        User.findById(userId).then().catch();
+        // send user feedback using res.json
+      }
+    })
+    .catch(err => {
+      // Forward validation errors on to the client, otherwise give a 500
+      // error because something unexpected has happened
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({code: 500, message: 'Internal server error'});
+    });
+});
 
 
 
@@ -50,7 +96,7 @@ router.post('/', jwtAuth, (req, res, next) => {
 //     });
 // });
 
-// /* ========== GET A POKEMON ========== */
+// /* ========== GET ANSWER FOR A POKEMON ========== */
 // router.post('/', (req, res, next) => {
 //   // const { id } = req.params;
 //   const { input, id } = req.body;
