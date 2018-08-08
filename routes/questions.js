0d809525bf6e6c19c1896d4b2765ddf9ff726dce
Albert Sare <thesundaymailman@gmail.com>
2018-08-08 11:29:38 -0700

    refactored questions to responed with a single question owned by user

--- routes/questions.js
+++ routes/questions.js
@@ -1,43 +1,70 @@
 'use strict';
 
 const express = require('express');
-const router = express.Router();
+const passport = require('passport');
 
+const router = express.Router();
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+
+const User = require('../models/user.js');
 const Question = require('../models/question.js');
+
+/* ========== GET A POKEMON ========== */
+router.post('/', jwtAuth, (req, res, next) => {
+  const { id } = req.body;
+  console.log(id);
+  return User
+    .findById(id)
+    .then(user => res.json(user.questions[0]))
+    .catch(err => next(err));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // mongoimport --uri MONGODB_URI --collection COLLECTION_NAME --file FILE_NAME --jsonArray
 
 /* ========== GET A POKEMON ========== */
-router.get('/:count', (req, res, next) => {
-  console.log(req.params.count);
-  return Question
-    .findOne().skip(Number(req.params.count))
-    .then(result=>res.json(result))
-    .catch(err => {
-      // Forward validation errors on to the client, otherwise give a 500
-      // error because something unexpected has happened
-      if (err.reason === 'ValidationError') {
-        return res.status(err.code).json(err);
-      }
-      res.status(500).json({code: 500, message: 'Internal server error'});
-    });
-});
+// router.get('/:count', (req, res, next) => {
+//   console.log(req.params.count);
+//   return Question
+//     .findOne().skip(Number(req.params.count))
+//     .then(result=>res.json(result))
+//     .catch(err => {
+//       // Forward validation errors on to the client, otherwise give a 500
+//       // error because something unexpected has happened
+//       if (err.reason === 'ValidationError') {
+//         return res.status(err.code).json(err);
+//       }
+//       res.status(500).json({code: 500, message: 'Internal server error'});
+//     });
+// });
 
-/* ========== GET A POKEMON ========== */
-router.post('/', (req, res, next) => {
-  // const { id } = req.params;
-  const { input, id } = req.body;
-  return Question
-    .findById(id)
-    .then(result => result.name === input ? res.json({bool: true, name: result.name}) : res.json({bool: false, name: result.name}))
-    .catch(err => {
-      // Forward validation errors on to the client, otherwise give a 500
-      // error because something unexpected has happened
-      if (err.reason === 'ValidationError') {
-        return res.status(err.code).json(err);
-      }
-      res.status(500).json({code: 500, message: 'Internal server error'});
-    });
-});
+// /* ========== GET A POKEMON ========== */
+// router.post('/', (req, res, next) => {
+//   // const { id } = req.params;
+//   const { input, id } = req.body;
+//   return Question
+//     .findById(id)
+//     .then(result => result.name === input ? res.json({bool: true, name: result.name}) : res.json({bool: false, name: result.name}))
+//     .catch(err => {
+//       // Forward validation errors on to the client, otherwise give a 500
+//       // error because something unexpected has happened
+//       if (err.reason === 'ValidationError') {
+//         return res.status(err.code).json(err);
+//       }
+//       res.status(500).json({code: 500, message: 'Internal server error'});
+//     });
+// });
 
 module.exports = router;
