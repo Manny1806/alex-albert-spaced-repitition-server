578dbfbd273e53b6ec19fe6f8f23d6e9e3c3b12c
Alex Widner <manny1806@gmail.com>
2018-08-08 18:47:19 -0700

    algo working

--- routes/questions.js
+++ routes/questions.js
@@ -29,44 +29,86 @@ router.post('/:id', jwtAuth, (req, res, next) => {
     .findById(id)
     // .then(result => result.name === input ? res.json({bool: true, name: result.name}) : res.json({bool: false, name: result.name}))
     .then(result => {
-      console.log("triggered")
+      console.log(result)
       resultPokemonName = result.name
       return result.name === input ? {isCorrect: true, answer: result.name} : {isCorrect: false, answer: result.name}})
     .then(answer => {
+      console.log("triggered")
       if (answer.isCorrect) {
         User
           .findById(userId)
           .then(user => {
-            let newQuestions = user
-            let newHead = newQuestions.questions[user.head].next;
-            newQuestions.questions[user.head].memoryStrength *= 2
-            newQuestions.questions[user.head].next = newQuestions.questions[newQuestions.questions[user.head].memoryStrength].next
-            // newQuestions.questions[user.head].next =  user.questions[user.head].memoryStrength * 2
-            // newQuestions.questions[user.head].memoryStrength *= 2
-            newQuestions.questions[newQuestions.questions[user.head].memoryStrength].next = user.head
-            newQuestions.head = newHead
-            console.log(newQuestions)
-            return User.findByIdAndUpdate(userId, {head: newHead, questions: newQuestions.questions})
+            console.log(user)
+            let currentQuestion = user.questions[user.head]
+            let currentIndex = user.head
+            currentQuestion.memoryStrength *= 2;
+            user.head = currentQuestion.next;
+            let insertAfterQuestion = currentQuestion;
+            let currQ = currentQuestion;
+              for (let i = 0; i < currentQuestion.memoryStrength; i++) {
+                let index = currQ.next;
+                if (currentQuestion.memoryStrength > user.questions.length) {
+                  currentQuestion.memoryStrength = user.questions.length - 1;
+                  index = user.questions.length - 1;
+                }
+                insertAfterQuestion = user.questions[index];
+                currQ = user.questions[currQ.next];
+              }
+
+              if (insertAfterQuestion.next === null) {
+                currentQuestion.next = null;
+              // otherwise set the currentQuestion to point to the node after the insertion point
+              } else {
+                currentQuestion.next = insertAfterQuestion.next;
+              }
+              // set the insertion point to point to the original index of the currentQuestion
+              insertAfterQuestion.next = currentIndex;
+              console.log(user)
+              user.save();
+            // }
+            // newQuestions.questions[user.head].next = newQuestions.questions[newQuestions.questions[user.head].memoryStrength].next
+            // newQuestions.questions[newQuestions.questions[user.head].memoryStrength].next = user.head
+            // newQuestions.head = newHead
+            // console.log(newQuestions)
+            return user
           })
-          .then(()=>res.json({bool: true, name: resultPokemonName}))
-          .catch();
+          .then((user)=>res.json({bool: true, name: resultPokemonName}))
+          .catch(err => next(err));
         // send user feedback using res.json
       } else {
         User
           .findById(userId)
           .then(user => {
-            let newQuestions = user
-            let newHead = newQuestions.questions[user.head].next;
-            newQuestions.questions[user.head].next =  user.questions[user.head].memoryStrength
-            newQuestions.questions[user.head].memoryStrength = 1
-            newQuestions.questions[newQuestions.questions[user.head].next].next = user.head
-            newQuestions.head = newHead
-            console.log(newQuestions)
-            return User.findByIdAndUpdate(userId, {head: newHead, questions: newQuestions.questions})
+            let currentQuestion = user.questions[user.head]
+            let currentIndex = user.head
+            currentQuestion.memoryStrength = 1;
+            user.head = currentQuestion.next;
+            let insertAfterQuestion = currentQuestion;
+            let currQ = currentQuestion;
+              for (let i = 0; i < currentQuestion.memoryStrength; i++) {
+                let index = currQ.next;
+                if (currentQuestion.memoryStrength > user.questions.length) {
+                  currentQuestion.memoryStrength = user.questions.length - 1;
+                  index = user.questions.length - 1;
+                }
+                insertAfterQuestion = user.questions[index];
+                currQ = user.questions[currQ.next];
+              }
+
+              if (insertAfterQuestion.next === null) {
+                currentQuestion.next = null;
+              // otherwise set the currentQuestion to point to the node after the insertion point
+              } else {
+                currentQuestion.next = insertAfterQuestion.next;
+              }
+              // set the insertion point to point to the original index of the currentQuestion
+              insertAfterQuestion.next = currentIndex;
+        
+              user.save();
+              return user
           })
-          .then(()=>res.json({bool: false, name: resultPokemonName}))
-          .catch();
-        // send user feedback using res.json
+            .then((user)=>res.json({bool: false, name: resultPokemonName}))
+          .catch(err => next(err));
       }
     })
     .catch(err => {
@@ -78,52 +120,5 @@ router.post('/:id', jwtAuth, (req, res, next) => {
       res.status(500).json({code: 500, message: 'Internal server error'});
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// mongoimport --uri MONGODB_URI --collection COLLECTION_NAME --file FILE_NAME --jsonArray
-
-/* ========== GET A POKEMON ========== */
-// router.get('/:count', (req, res, next) => {
-//   console.log(req.params.count);
-//   return Question
-//     .findOne().skip(Number(req.params.count))
-//     .then(result=>res.json(result))
-//     .catch(err => {
-//       // Forward validation errors on to the client, otherwise give a 500
-//       // error because something unexpected has happened
-//       if (err.reason === 'ValidationError') {
-//         return res.status(err.code).json(err);
-//       }
-//       res.status(500).json({code: 500, message: 'Internal server error'});
-//     });
-// });
-
-// /* ========== GET ANSWER FOR A POKEMON ========== */
-// router.post('/', (req, res, next) => {
-//   // const { id } = req.params;
-//   const { input, id } = req.body;
-//   return Question
-//     .findById(id)
-//     .then(result => result.name === input ? res.json({bool: true, name: result.name}) : res.json({bool: false, name: result.name}))
-//     .catch(err => {
-//       // Forward validation errors on to the client, otherwise give a 500
-//       // error because something unexpected has happened
-//       if (err.reason === 'ValidationError') {
-//         return res.status(err.code).json(err);
-//       }
-//       res.status(500).json({code: 500, message: 'Internal server error'});
-//     });
-// });
 
 module.exports = router;
