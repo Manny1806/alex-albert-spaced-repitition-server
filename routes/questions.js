0064e4e6d5e43a424121a0e215762adf409cfc7b
Alex Widner <manny1806@gmail.com>
2018-08-09 11:54:30 -0700

    changed post to get

--- routes/questions.js
+++ routes/questions.js
@@ -10,8 +10,8 @@ const User = require('../models/user.js');
 const Question = require('../models/question.js');
 
 /* ========== GET A POKEMON ========== */
-router.post('/', jwtAuth, (req, res, next) => {
-  const { id } = req.body;
+router.get('/:id', jwtAuth, (req, res, next) => {
+  const { id } = req.params;
   return User
     .findById(id)
     .then(user => {
@@ -92,39 +92,6 @@ router.post('/:id', jwtAuth, (req, res, next) => {
     });
 });
 
-// mongoimport --uri MONGODB_URI --collection COLLECTION_NAME --file FILE_NAME --jsonArray
 
-/* ========== GET A POKEMON ========== */
-// router.get('/:count', (req, res, next) => {
-//   console.log(req.params.count);
-//   return Question
-//     .findOne().skip(Number(req.params.count))
-//     .then(result=>res.json(result))
-//     .catch(err => {
-//       // Forward validation errors on to the client, otherwise give a 500
-//       // error because something unexpected has happened
-//       if (err.reason === 'ValidationError') {
-//         return res.status(err.code).json(err);
-//       }
-//       res.status(500).json({code: 500, message: 'Internal server error'});
-//     });
-// });
-
-// /* ========== GET ANSWER FOR A POKEMON ========== */
-// router.post('/', (req, res, next) => {
-//   // const { id } = req.params;
-//   const { input, id } = req.body;
-//   return Question
-//     .findById(id)
-//     .then(result => result.name === input ? res.json({bool: true, name: result.name}) : res.json({bool: false, name: result.name}))
-//     .catch(err => {
-//       // Forward validation errors on to the client, otherwise give a 500
-//       // error because something unexpected has happened
-//       if (err.reason === 'ValidationError') {
-//         return res.status(err.code).json(err);
-//       }
-//       res.status(500).json({code: 500, message: 'Internal server error'});
-//     });
-// });
 
 module.exports = router;
